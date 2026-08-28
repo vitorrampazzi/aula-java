a7afa838b179d69087a2c67242b1b02a993f64b4
Vitor Rampazzi Franco <vitor.franco@math.group>
2026-08-28 15:50:41 -0300

    exercicio inacabado

---
+++ java/testes/conta.java
@@ -0,0 +1,45 @@
+public class conta {
+private int id;
+private double saldo;
+private String email;
+private String sexo;
+
+// Getters e Setters
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public double getSaldo() {
+    return saldo;
+}
+
+public void setSaldo(double saldo) {
+    this.saldo = saldo;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getSexo() {
+    return sexo;
+}
+
+public void setSexo(String sexo) {
+    this.sexo = sexo;
+}
+
+
+public void exibirInformacoees(){
+    
+}
+}
